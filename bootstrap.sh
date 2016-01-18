db2da8f3353e9076b3f00e57820d9711163288e2
Roman Pushkin <roman@breakthrough.com>
2016-01-18 14:31:47 -0800

    Improve post-provision instructions

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,5 +3,4 @@ sudo apt-get update &&\
 sudo apt-get install -y build-essential &&\
 source /vagrant/rubydevenv-install.sh &&\
 echo bootstrap ok
-echo Now go to project directory, type "bundle install" and "rails s" to kick off the server on port 3000.
-
+echo now go to project directory \(\"cd /vagrant/www\"\), then type \"bundle install \&\& rake db:migrate \&\& rails s -b 0.0.0.0\" to kick off the server on port 3000
